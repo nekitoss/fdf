f8e916d120a7729c382f9d536a47d1b4551bf5b7
Nick <nekit_p@ukr.net>
2017-05-04 21:31:04 +0300

    draw with putimage, lines draws wrong after overzoom

--- fdf.c
+++ fdf.c
@@ -1,28 +1,22 @@
 #include "h_fdf.h"
 
-#define FLOAT_T double
-
-typedef struct	s_fdf
+void printBits(size_t const size, void const * const ptr)
 {
-	int		fd;
-	FLOAT_T	**arr;
-	char	**text;
-	char	*line;
-	int		ret;
-	size_t	num_rows;
-	size_t	num_cols;
-	char	*addr;
-	void	*mlx_ptr;
-	size_t	size_x;
-	size_t	size_y;
-	void	*win_ptr;
-	int		zoom;
-	size_t	x1;
-	size_t	y1;
-	size_t	x2;
-	size_t	y2;
-	
-}				t_fdf;
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+        printf(" ");
+    }
+    puts("");
+}
 
 void		error(char *str)
 {
@@ -38,16 +32,16 @@ void		error_msg(char *str)
 
 void		print_struct(t_fdf *ls)
 {
-	size_t len;
-	size_t i;
-	size_t j;
+	int len;
+	int i;
+	int j;
 
 	i = 0;
 	len = 0;
 	printf("Struct ----------------------\n");
 	printf("fd = %d\n", ls->fd);
-	printf("cols = %zu\n", ls->num_cols);
-	printf("rows = %zu\n", ls->num_rows);
+	printf("cols = %d\n", ls->num_cols);
+	printf("rows = %d\n", ls->num_rows);
 	printf("*arr=\n");
 	len = ft_arrlen((void **)ls->arr);
 	while (i < len)
@@ -93,36 +87,7 @@ void		del_struct(t_fdf *ls)
 	// ft_arrdel((void ***)(&(ls->text)));
 }
 
-void		draw_line(t_fdf *ls)
-{
-	int			error;
-	int			error2;
-	int			delta_y;
-	int			delta_x;
-
-	// printf("drw=%zu:%zu; %zu:%zu\n", ls->x1, ls->y1, ls->x2, ls->y2);
-	delta_y = ABS(ls->y2 - ls->y1);
-	delta_x = ABS(ls->x2 - ls->x1);
-	error = delta_x - delta_y;
-	error2 = error * 2;
-	while (ls->x1 != ls->x2 || ls->y1 != ls->y2)
-	{
-		mlx_pixel_put(ls->mlx_ptr, ls->win_ptr, ls->x1 + 10,
-			ls->y1 + 10, 0x000000FF);
-		if (error2 > -delta_y)
-		{
-			error -= delta_y;
-			ls->x1 += (ls->x1 < ls->x2 ? 1 : -1);
-		}
-		if (error2 < delta_x)
-		{
-			error += delta_x;
-			ls->y1 += (ls->y1 < ls->y2 ? 1 : -1);
-		}
-	}
-}
-
-/*void		coun_coord(t_fdf *ls, size_t i, size_t j)
+/*void		coun_coord(t_fdf *ls, int i, int j)
 {
 	ls->y1 = i * ls->zoom ;
 	ls->x1 = j * ls->zoom ;
@@ -138,8 +103,8 @@ void		draw_line(t_fdf *ls)
 
 void		print_lines(t_fdf *ls)
 {
-	size_t	i;
-	size_t	j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (i < ls->num_rows && i < ls->size_x)
@@ -167,13 +132,13 @@ void		print_lines(t_fdf *ls)
 	}
 }
 
-void		convert_row(t_fdf *ls, char **row, size_t r_num)
+void		convert_row(t_fdf *ls, char **row, int r_num)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	if (ls->num_cols &&
-		ls->num_cols == ft_arrlen((void **)row))
+		ls->num_cols == (int)ft_arrlen((void **)row))
 	{
 		ls->arr[r_num] = (FLOAT_T *)ft_memalloc(sizeof(FLOAT_T) * ls->num_cols);
 		while (i < ls->num_cols)
@@ -189,7 +154,7 @@ void		convert_row(t_fdf *ls, char **row, size_t r_num)
 
 void		rows_into_array(t_fdf *ls)
 {
-	size_t	i;
+	int	i;
 	char	**tmp;
 
 	i = 0;
@@ -206,7 +171,7 @@ void		rows_into_array(t_fdf *ls)
 
 void		count_save_rows(t_fdf *ls)
 {
-	size_t	i;
+	int	i;
 
 	ls->fd = open(ls->addr, O_RDONLY);
 	while ((ls->ret = get_next_line(ls->fd, &(ls->line))) > 0)
@@ -232,8 +197,8 @@ void		count_save_rows(t_fdf *ls)
 
 void		print_in_window(t_fdf *ls)
 {
-	size_t	i;
-	size_t	j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (i < ls->num_rows && i < ls->size_x)
@@ -245,7 +210,7 @@ void		print_in_window(t_fdf *ls)
 			{
 				mlx_pixel_put(ls->mlx_ptr, ls->win_ptr, j * ls->zoom + 10,
 					i * ls->zoom + 10, 0x0000FFFF);
-				// printf("put pixel: x=%zu y=%zu\n", i, j);
+				// printf("put pixel: x=%d y=%d\n", i, j);
 			}
 			else
 				mlx_pixel_put(ls->mlx_ptr, ls->win_ptr,
@@ -256,20 +221,6 @@ void		print_in_window(t_fdf *ls)
 	}
 }
 
-void		zoom_reset(t_fdf *ls)
-{
-	ls->zoom = 1;
-	mlx_clear_window(ls->mlx_ptr, ls->win_ptr);
-	print_in_window(ls);
-}
-
-void		zoom_in(t_fdf *ls)
-{
-	ls->zoom += 5;
-	mlx_clear_window(ls->mlx_ptr, ls->win_ptr);
-	print_in_window(ls);
-}
-
 void		zoom(t_fdf *ls, int step)
 {
 	if (step > 0)
@@ -278,9 +229,33 @@ void		zoom(t_fdf *ls, int step)
 		ls->zoom += step;
 	else
 		ls->zoom = 1;
+	printf("zoom=%d\n", ls->zoom);
+	redraw(ls);
+	// exit(1);
+}
+
+void		redraw(t_fdf *ls)
+{
+	// printf("clear\n");
 	mlx_clear_window(ls->mlx_ptr, ls->win_ptr);
-	print_in_window(ls);
-	print_lines(ls);
+	// printf("destroy\n");
+	if (ls->img_ptr)
+		mlx_destroy_image(ls->mlx_ptr, ls->img_ptr);
+	// printf("new_img %dx%d\n", (int)ls->size_x - 19, (int)ls->size_y - 19);
+	ls->img_ptr = mlx_new_image(ls->mlx_ptr, (int)ls->size_x - 19, (int)ls->size_y - 19);
+	if (!(ls->img_ptr))
+		error("IMG_not created");
+	// printf("get_data\n");
+	ls->data = mlx_get_data_addr(ls->img_ptr, &(ls->bits_per_pixel), &(ls->size_line), &(ls->endian));
+	// printf("make_image\n");
+	make_image(ls);
+	// printf("make_lines\n");
+	make_lines(ls);
+	// printf("put image\n");
+	mlx_put_image_to_window(ls->mlx_ptr, ls->win_ptr, ls->img_ptr, 10, 10);
+	
+	// print_in_window(ls);
+	// print_lines(ls);
 }
 
 int			key_f(int key, void *ls_void)
@@ -311,31 +286,146 @@ int			test(void *ls_void)
 	return (0);
 }
 
+void		draw_line(t_fdf *ls)
+{
+	int			error;
+	int			error2;
+	int			delta_y;
+	int			delta_x;
+
+	// printf("drw=%d:%d; %d:%d\n", ls->x1, ls->y1, ls->x2, ls->y2);
+	delta_y = ABS(ls->y2 - ls->y1);
+	delta_x = ABS(ls->x2 - ls->x1);
+	error = delta_x - delta_y;
+	error2 = error * 2;
+	while (ls->x1 != ls->x2 || ls->y1 != ls->y2)
+	{
+		// mlx_pixel_put(ls->mlx_ptr, ls->win_ptr, ls->x1 + 10,
+			// ls->y1 + 10, 0x000000FF);
+		pixel_to_img(ls, ls->y1, ls->x1, 0x000000FF);
+		// if ( ls->y1 == 1 && ls->x1 < 55)
+			// printf("%d:%d ", ls->y1, ls->x1);
+		if (error2 > -delta_y)
+		{
+			error -= delta_y;
+			ls->x1 += (ls->x1 < ls->x2 ? 1 : -1);
+		}
+		if (error2 < delta_x)
+		{
+			error += delta_x;
+			ls->y1 += (ls->y1 < ls->y2 ? 1 : -1);
+		}
+	}
+	// printf("\n");
+}
+
+void		pixel_to_img(t_fdf *ls, size_t i, size_t j, int color)
+{
+	int *tmp;
+
+	tmp = (int *)&(ls->data[IMG_ROW(i) + IMG_COL(j)]);
+	*tmp = color;
+}
+
+void		make_image(t_fdf *ls)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	while (i < ls->num_rows && (i * ls->zoom) < ls->size_y - 19)
+	{
+		j = 0;
+		while (j < ls->num_cols && (j * ls->zoom) < ls->size_x - 19)
+		{
+			// printf("%d:%d ", i * ls->zoom, j * ls->zoom);
+			if (ls->arr[i][j])
+				pixel_to_img(ls, (i * ls->zoom), (j * ls->zoom), 0x0000FFFF);
+			else
+				pixel_to_img(ls, (i * ls->zoom), (j * ls->zoom), 0x00FF0000);
+			j++;
+		}
+		// printf("\n");
+		i++;
+	}
+}
+
+void		make_lines(t_fdf *ls)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < ls->num_rows && (i * ls->zoom) < ls->size_y - 19)
+	{
+		j = 0;
+		while (j < ls->num_cols && (j * ls->zoom) < ls->size_x - 19)
+		{
+			// if (ls->arr[i][j] > 1)
+			{
+			ls->y1 = i * ls->zoom ;
+			ls->x1 = j * ls->zoom ;
+			ls->x2 = (j + ((j < ls->num_cols - 1) ? 1 : 0)) * ls->zoom ;
+			ls->y2 = (i) * ls->zoom ;			
+			draw_line(ls);
+			ls->y1 = i * ls->zoom ;
+			ls->x1 = j * ls->zoom ;
+			ls->x2 = (j) * ls->zoom ;
+			ls->y2 = (i + ((i < ls->num_rows - 1) ? 1 : 0)) * ls->zoom ;
+			draw_line(ls);
+			j++;
+			}
+		}
+		i++;
+	}
+}
+
 int			main(int argc, char **argv)
 {
 	t_fdf	*ls;
+	// t_img	*pixel;
+	// int		*test;
 
 	ls = (t_fdf *)ft_memalloc(sizeof(t_fdf));
 	if (argc == 2)
 	{
 		ls->addr = argv[1];
 		count_save_rows(ls);
-		// printf("rows = %zu\n", ls->num_rows);
+		// printf("rows = %d\n", ls->num_rows);
 		rows_into_array(ls);
 		// print_struct(ls);
 		ls->mlx_ptr = mlx_init();
 		ls->zoom = 50;
 		ls->size_x = MIN(2560, (ls->num_cols - 1) * (ls->zoom) + 20);
 		ls->size_y = MIN(1315, (ls->num_rows - 1) * (ls->zoom) + 20);
-		printf("window %zu x %zu\n", ls->size_x, ls->size_y);
+		printf("window %d x %d\n", ls->size_x, ls->size_y);
 		if (ls->mlx_ptr)
 		{
 			ls->win_ptr = mlx_new_window(ls->mlx_ptr, ls->size_x, ls->size_y,
 				ls->addr);
 			if (ls->win_ptr)
 			{
-				print_in_window(ls);
-				print_lines(ls);
+
+				printf("num_cols=%d, num_rows=%d\n", ls->num_cols, ls->num_rows);
+				// test = (int *)&(ls->data[IMG_ROW(1) + IMG_COL(1)]);
+				// *test = 0x00FFFFFF;
+				// pixel = (t_img *)&(ls->data[IMG_ROW(1) + IMG_COL(1)]);
+				// pixel->t = 0x00;
+				// pixel->r = 0x00;
+				// pixel->g = 0xFF;
+				// pixel->b = 0x00;
+				// printf("%p = %p\n", mlx_get_data_addr(ls->img_ptr, &(ls->bits_per_pixel), &(ls->size_line), &(ls->endian)), ls->data);
+				redraw(ls);
+				printf("bits pp=%d(bits)=%d(bytes), size_line=%d(bytes), endian=%d\n", ls->bits_per_pixel, ls->bits_per_pixel/8,ls->size_line, ls->endian);
+				// make_image(ls);
+				// make_lines(ls);
+				// printBits (ls->size_line*ls->num_rows, ls->data);
+				
+				// mlx_put_image_to_window(ls->mlx_ptr, ls->win_ptr, ls->img_ptr, 10, 10);
+				
+				// mlx_do_key_autorepeaton(ls->mlx_ptr);
+				// print_in_window(ls);
+				// print_lines(ls);
 				mlx_key_hook(ls->win_ptr, key_f, (void *)ls);
 				// mlx_expose_hook (ls->win_ptr, test, (void *)ls);
 				mlx_loop(ls->mlx_ptr);
--- h_fdf.h
+++ h_fdf.h
@@ -3,6 +3,66 @@
 // # include "assert.h"
 
 # include "libft/libft.h"
-# include <mlx.h>
+// # include <mlx.h>
+#include "minilibx_macos/mlx.h"
+
+# define FLOAT_T float
+# define IMG_ROW(a) (a * (ls->size_line))
+# define IMG_COL(b) (b * (ls->bits_per_pixel / 8))
+
+typedef struct	s_fdf
+{
+	int		fd;
+	FLOAT_T	**arr;
+	char	**text;
+	char	*line;
+	int		ret;
+	int		num_rows;
+	int		num_cols;
+	char	*addr;
+	void	*mlx_ptr;
+	int		size_x;
+	int		size_y;
+	void	*win_ptr;
+	int		zoom;
+	int		x1;
+	int		y1;
+	int		x2;
+	int		y2;
+	void	*img_ptr;
+	int		bits_per_pixel;
+	int		endian;
+	int		size_line;
+	char	*data;
+}				t_fdf;
+
+typedef struct	s_img
+{
+	char	b;
+	char	g;
+	char	r;
+	char	t;
+}				t_img;
+
+void		printBits(size_t const size, void const * const ptr);
+void		error(char *str);
+void		error_msg(char *str);
+void		print_struct(t_fdf *ls);
+void		del_struct(t_fdf *ls);
+void		draw_line(t_fdf *ls);
+void		print_lines(t_fdf *ls);
+void		convert_row(t_fdf *ls, char **row, int r_num);
+void		rows_into_array(t_fdf *ls);
+void		count_save_rows(t_fdf *ls);
+void		print_in_window(t_fdf *ls);
+void		zoom_reset(t_fdf *ls);
+void		zoom_in(t_fdf *ls);
+void		zoom(t_fdf *ls, int step);
+int			key_f(int key, void *ls_void);
+int			test(void *ls_void);
+void		make_image(t_fdf *ls);
+void		make_lines(t_fdf *ls);
+void		pixel_to_img(t_fdf *ls, size_t i, size_t j, int color);
+void		redraw(t_fdf *ls);
 
 #endif
